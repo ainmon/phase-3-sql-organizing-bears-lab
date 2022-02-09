510534a917746142981122e92c6b256b7f2a2dad
Adam <inmon.adam@yahoo.com>
2022-02-09 16:52:50 -0600

    commit tree 0e3b4f1c43fa22565a9d9ec100adec9e01d6c5ab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 53, 'M', 'brown', 'wacky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
